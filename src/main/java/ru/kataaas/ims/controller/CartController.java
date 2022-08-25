3589aaac583a05e138649631e5698ccd859f0970
kataaas <kataaas@yandex.ru>
2022-08-25 13:57:17 +0300

    Updated
    Signed-off-by: kataaas <kataaas@yandex.ru>

--- src/main/java/ru/kataaas/ims/controller/CartController.java
+++ src/main/java/ru/kataaas/ims/controller/CartController.java
@@ -1,7 +1,10 @@
 package ru.kataaas.ims.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kataaas.ims.dto.CartDTO;
 import ru.kataaas.ims.entity.CartEntity;
@@ -9,6 +12,7 @@ import ru.kataaas.ims.mapper.CartMapper;
 import ru.kataaas.ims.service.CartService;
 
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -27,6 +31,16 @@ public class CartController {
     public CartDTO fetchCart() {
         Optional<CartEntity> cart = cartService.fetchByUserId(1L);
         return cart.map(cartMapper::toCartDTO).orElseGet(() -> null);
+    }
+
+    @GetMapping("/buy")
+    public ResponseEntity<?> orderProducts(@RequestParam("id") Set<Long> ids) {
+        try {
+            cartService.orderProducts(ids, 1L);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+        }
     }
 
 }
---
+++ src/main/java/ru/kataaas/ims/controller/OrderController.java
@@ -0,0 +1,10 @@
+package ru.kataaas.ims.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/order")
+public class OrderController {
+
+}
