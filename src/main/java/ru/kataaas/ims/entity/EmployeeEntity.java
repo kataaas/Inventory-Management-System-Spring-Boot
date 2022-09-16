afe370bd74f5049a74e734b631927047c72a7016
kataaas <kataaas@yandex.ru>
2022-09-16 20:51:18 +0300

    Creating an employee
    Signed-off-by: kataaas <kataaas@yandex.ru>

---
+++ src/main/java/ru/kataaas/ims/entity/EmployeeEntity.java
@@ -0,0 +1,43 @@
+package ru.kataaas.ims.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String login;
+
+    @NotNull
+    @Size(min = 8)
+    private String password;
+
+    @NotNull
+    @Column(unique = true)
+    @Pattern(regexp = "\\+7-\\d{3}-\\d{3}-\\d{2}-\\d{2}")
+    private String phoneNumber;
+
+    @NotNull
+    private String post;
+
+    @CreationTimestamp
+    private Date lastModifiedDateTime;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+}
---
+++ src/main/java/ru/kataaas/ims/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package ru.kataaas.ims.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.kataaas.ims.entity.EmployeeEntity;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
